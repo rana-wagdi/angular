7bfe361f805b795dd55df0aa4976dad26bf079ba
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2022-02-23 15:22:29 +0200

    practice project

---
+++ practice/practice/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent {
+  password =''
+
+
+ onButtonClick(){
+  this.password="MY PASSWORD!!"
+  console.log('Button was Clicked')
+ }
+}
