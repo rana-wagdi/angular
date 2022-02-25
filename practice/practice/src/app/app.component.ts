bb6b3f21bc46b93e410c4483b381b725f8b0fb73
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2022-02-25 20:59:20 +0200

    start app

--- practice/practice/src/app/app.component.ts
+++ practice/practice/src/app/app.component.ts
@@ -3,17 +3,69 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
-
-
 export class AppComponent {
-  password =''
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbels = false;
+  password = '';
 
 
- onButtonClick(){
-  this.password="MY PASSWORD!!"
-  console.log('Button was Clicked')
- }
+  
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbels() {
+    this.includeSymbels = !this.includeSymbels;
+  }
+
+  onButtonClick() {
+    // console.log(`About to generate a password with the follweing:
+    // Includes Letter: ${this.includeLetters}`);
+    // this.password = 'MY PASSWORD!!';
+    // console.log('Button was Clicked');
+    
+    const numbers ='1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    
+    let vaildChars = '';
+    
+    if(this.includeLetters){
+        
+      vaildChars += letters;
+    }
+    if(this.includeNumbers){
+      
+      vaildChars += numbers
+    }
+    if(this.includeSymbels){
+              
+      vaildChars += symbols
+    }
+    
+    let generatedPassword = "";
+    
+    for(let i = 0; i< this.length; i++){
+      const index = Math.floor(Math.random() * vaildChars.length)
+      
+      generatedPassword += vaildChars[index]
+    }
+    
+   this.password = generatedPassword;
+
+  }
 }
