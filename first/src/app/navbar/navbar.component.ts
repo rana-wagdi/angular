7c3a4f1e5061baf449cdd09ff7a30645578c9fc6
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2022-02-25 23:52:22 +0200

    initial commit

--- first/src/app/navbar/navbar.component.ts
+++ first/src/app/navbar/navbar.component.ts
@@ -9,6 +9,15 @@ export class NavbarComponent implements OnInit {
 
 
   name : string = "Rana";
+  
+  isAdmin: boolean = false;
+  userNames : Array<string> = [
+  
+  "Ahmed", "Karim", "Rana"]
+  
+  grade : number = 4;
+  
+  
   constructor() { }
 
   ngOnInit(): void {
